4864a11b5a7be8570c09d9f1e5eba61e87b7cde3
lsebalj1 <lusebalj@student.unipu.hr>
2024-03-21 18:09:14 +0100

    Povezivanje s backendom

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+
 module.exports = defineConfig({
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001', // Your Express.js backend URL
+        changeOrigin: true,
+        pathRewrite: { '^/api': '' },
+      },
+    },
+  },
   transpileDependencies: true
-})
+});
